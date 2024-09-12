077eaef197d5c24b5eb86e2e27321896291511f2
W-ptr <wisnu.putra001@binus.ac.id>
2024-09-12 23:46:31 +0700

    feat: add redis

---
+++ database/redis.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"context"
+	"log"
+	"github.com/redis/go-redis/v9"
+)
+
+func getRedisConnection()(*redis.Client,error){
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+	ctx := context.Background()
+	pong,err := rdb.Ping(ctx).Result()
+	if err != nil{
+		return nil,err
+	}
+	log.Println("connected to redis: ",pong)
+	return rdb,nil
+}
